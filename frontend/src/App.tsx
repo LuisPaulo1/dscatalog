d0d2c3615e4f379c7f34fa0f5bd037d61d5e6646
LuisPaulo1 <csluispaulo@gmail.com>
2021-08-31 18:49:35 -0300

    Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-import './assets/styles/custom.scss'
+import NavBar from './components/Navbar';
+import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello DSCatalog</h1>
-  );
+    <>
+      <NavBar />
+      <h1>Hello DSCatalog</h1>
+    </>
+  )
 }
 
 export default App;
