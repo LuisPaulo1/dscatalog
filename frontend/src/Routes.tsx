956096a4259b31674f004689df553469cb635e50
LuisPaulo1 <csluispaulo@gmail.com>
2021-09-04 11:40:52 -0300

    ProductDetails route

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from 'Pages/Home';
+import Home from 'pages/Home';
 import Navbar from 'components/Navbar';
-import Catalog from 'Pages/Catalog';
-import Admin from 'Pages/Admin';
+import Catalog from 'pages/Catalog';
+import Admin from 'pages/Admin';
+import ProductDetails from 'pages/ProductDetails'
 
 const Routes = () => (
   <BrowserRouter>
@@ -11,8 +12,11 @@ const Routes = () => (
       <Route path="/" exact>
         <Home />
       </Route>
-      <Route path="/products">
+      <Route path="/products" exact>
         <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
       </Route>
       <Route path="/admin">
         <Admin />
