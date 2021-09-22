e464cbdfba195e193d5c10642f3ef9fc6568d135
LuisPaulo1 <csluispaulo@gmail.com>
2021-09-22 19:07:58 -0300

    Private Route

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -90,7 +90,7 @@ axios.interceptors.response.use(
     return response;
   },
   function (error) {
-    if (error.response.status === 401) {
+    if (error.response.status === 401 || error.response.status === 403) {
       history.push('/admin/auth');
     }
     return Promise.reject(error);
