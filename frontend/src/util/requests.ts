c86b25e5fe7765e1acb57219de6288705e22a2fd
LuisPaulo1 <csluispaulo@gmail.com>
2021-09-23 17:03:14 -0300

    hasAnyRoles

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -113,3 +113,18 @@ export const isAuthenticated = (): boolean => {
   let tokenData = getTokenData();
   return tokenData && tokenData.exp * 1000 > Date.now() ? true : false;
 };
+
+export const hasAnyRoles = (roles: Role[]) : boolean => {
+
+  if(roles.length === 0) {
+    return true;
+  }
+
+  const tokenData = getTokenData();
+
+  if(tokenData !== undefined) {
+    return roles.some(role => tokenData.authorities.includes(role));
+  }
+
+  return false;
+}
