678fd64b26f92e713d54150d5fa5b47ceca15d88
LuisPaulo1 <csluispaulo@gmail.com>
2022-02-02 18:00:51 -0300

    REFACTOR token, Role

--- frontend/src/util/__tests__/formatters.spec.ts
+++ frontend/src/util/__tests__/formatters.spec.ts
@@ -1,13 +1,45 @@
 import { formatPrice } from '../formatters';
- 
-test('formatPrice should format number pt-BR when given 10.1', () => {
-  
-  // ARRANGE  
-  const value = 10.1;
-  
-  // ACT
-  const result = formatPrice(value);
- 
-  // ASSERT
-  expect(result).toEqual('10,10');
+
+describe('formatPrice for positive numbers', () => {
+
+    test('should format number pt-BR when given 10.1', () => {
+      // ARRANGE
+      const value = 10.1;
+      // ACT
+      const result = formatPrice(value);
+      // ASSERT
+      expect(result).toEqual('10,10');
+    });
+
+    test('should format number pt-BR when given 0.1', () => {
+        // ARRANGE
+        const value = 0.1;
+        // ACT
+        const result = formatPrice(value);
+        // ASSERT
+        expect(result).toEqual('0,10');
+    });
+
+});
+
+describe('formatPrice for non-positive numbers', () => {
+
+    test('should format number pt-BR when given 0', () => {
+      // ARRANGE
+      const value = 0;
+      // ACT
+      const result = formatPrice(value);
+      // ASSERT
+      expect(result).toEqual('0,00');
+    });
+
+    test('should format number pt-BR when given -5.1', () => {
+        // ARRANGE
+        const value = -5.1;
+        // ACT
+        const result = formatPrice(value);
+        // ASSERT
+        expect(result).toEqual('-5,10');
+    });
+
 });
